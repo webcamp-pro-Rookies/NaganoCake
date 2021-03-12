dba8f2d3b148b6e4c66e6451f050badb6d4ff677
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-12 05:16:08 +0000

    Queryプラグインを導入, Gemを導入, モデルを編集, コントローラーを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+    :email,
+    :name,
+    :postcode,
+    :prefecture_name,
+    :address_city,
+    :address_street,
+    :address_building
+  ])
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  # 都道府県コードから都道府県名に自動で変換する。
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
+  # ~.prefecture_nameで都道府県名を参照出来る様にする。
+
+
+
 end

34c901118b366505a61d67b8622d5222c1d38eba
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-12 13:50:37 +0000

    shipping_address_view

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,2 +1,29 @@
 class ShippingAddressesController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+
+  def configure_permitted_parameters
+    shipping_address_parameter_sanitizer.permit(
+      :user_id,
+      :destination_name,
+      :postcode,
+      :prefecture_name,
+      :address_city,
+      :address_street,
+      :address_building
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
     controllers: { registrations: 'registrations' }
   resources :users, only: [:show, :destroy, :quit]
 #----------Products--------------
-  root to: "products#top" 
+  root to: "products#top"
   get 'about' => 'products#about'
   resources :products, only: [:index, :show]
 #--------------------------------
-  
+#----------Shipping_address--------------
+  resources :shipping_addresses, only: [:index, :create, :edit, :destroy]
+#--------------------------------
 end

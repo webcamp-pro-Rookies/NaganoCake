0b2819e103cb60ac06f4b3667f3ddd92d235a157
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-13 11:15:35 +0000

    カラム整理

--- app/controllers/shipping_addresses_controller.rb
+++ app/controllers/shipping_addresses_controller.rb
@@ -1,29 +1,40 @@
 class ShippingAddressesController < ApplicationController
 
   def index
+    # @shipping_addresses = current_user.shipping_address
+    @shipping_address = ShippingAddress.new
   end
 
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    # @shipping_address = ShippingAddress.find_by(params[:id])
+    @shipping_address.save
+    redirect_to shipping_addresses_path
   end
 
   def edit
+    @shipping_address = ShippingAddress(params[:id])
+    if @shipping_address.update(shipping_address_params)
+      redirect_to shipping_addresses_path(@shipping_address)
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
 
+  private
 
-
-  def configure_permitted_parameters
-    shipping_address_parameter_sanitizer.permit(
-      :user_id,
+  def shipping_address_params
+    params.require(:shipping_address).permit(
       :destination_name,
       :postcode,
       :prefecture_name,
       :address_city,
       :address_street,
-      :address_build
-    )
+      :address_building
+      )
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :carts
   has_many :orders
-  has_many :shipping_address
-         
+  # has_many :shipping_addresses
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :ruby_last_name, presence: true
@@ -15,13 +15,13 @@ class User < ApplicationRecord
   validates :address_city, presence: true
   validates :address_street, presence: true
   validates :phone_number, presence: true
-  
+
   def update_without_current_password(params, *options)
     if params[:password].blank? || params[:password_confirmation].blank?
       params.delete(:password)
       params.delete(:password_confirmation)
     end
-    
+
     result = update_attributes(params, *options)
     clean_up_passwords
     result

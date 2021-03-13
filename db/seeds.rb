d62207e8e2b1f5d7184932a24c32a7131ff99660
zakinao <zakinaororo@gmail.com>
2021-03-13 02:06:32 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+              last_name: "田中",
+              first_name: "太郎",
+              ruby_last_name: "タナカ",
+              ruby_first_name: "タロウ",
+              phone_number: "11111111111",
+              email: "test@example.com",
+              password: "password",
+              postcode: "1020082",
+              prefecture_code: "1",
+              address_city: "新宿区",
+              address_street: "2丁目",
+              address_building: "",
+              user_status: true,
+              admin: false
+                )

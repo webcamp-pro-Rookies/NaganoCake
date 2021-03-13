3987e6ef2916f2612053f566f8d899fe15e95976
missi0415 <missi0415@gmail.com>
2021-03-13 06:34:41 +0000

    proseed

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   has_many :carts
   has_many :ordered_products
   belongs_to :category
+  
+  #------enumで数値指定-------
+  enum sales_status: { 販売中: 0, 売切:1 ,準備中: 2 }
+  #---------------------------
 end

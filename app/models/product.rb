299d2ea6a906291b519fdc2120d0ed6083831061
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-14 13:31:03 +0000

    フォームにカテゴリのリストを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ApplicationRecord
-  has_many :carts
-  has_many :ordered_products
+  # has_many :carts
+  # has_many :ordered_products
   belongs_to :category
 
   attachment :image
+
+  def self.get_Categories_list
+    category_str = Category.all.pluck(:category_name)
+    cate_hash = (category_str).zip(0...category_str.size)
+  end
+
 end

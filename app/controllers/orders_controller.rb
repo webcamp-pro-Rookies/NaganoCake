acab5430f1aa64c81b62d7a87fdfc87162cb528c
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-03-14 07:35:06 +0000

    order_index 部分テンプレート 未完成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,17 @@
 class OrdersController < ApplicationController
   def index
+    @orders = current_user.orders
   end
 
   def log
+    @order = User.order
   end
 
   def new
   end
 
   def show
+    @order = User.order(current_user)
   end
 
   def thanks

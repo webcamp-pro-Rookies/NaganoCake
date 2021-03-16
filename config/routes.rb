295487866813fcce56ebb60e95399946577d2b5c
zakinao <zakinaororo@gmail.com>
2021-03-16 07:54:29 +0000

    エラーメッセージ日本語化　カスタマー周り修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   devise_for :customers, :controllers => {
     :sessions => 'customers/sessions',
     :registrations => 'customers/registrations',
-    :passwords => 'customers/passwords'
   }
   devise_for :admins,:controllers => {
     :sessions => 'admins/sessions',
     :registrations => 'admins/registrations',
-    :passwords => 'admins/passwords'
    }
 
 #----------customer--------------

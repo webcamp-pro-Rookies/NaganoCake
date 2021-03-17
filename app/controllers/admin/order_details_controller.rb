53f88df9090d1a2bb01efa7f4c7d3730adfc19b1
zakinao <zakinaororo@gmail.com>
2021-03-17 07:02:58 +0000

    管理者　注文ステータス変更　ボタン実装

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,9 +1,9 @@
-class Admin::OrederDetailsController < ApplicationController
+class Admin::OrderDetailsController < ApplicationController
     before_action :authenticate_admin!
   def update
   		@order_detail = OrderDetail.find(params[:id])
 		  if @order_detail.update(order_detail_params)
-		  	flash[:success] = "制作ステータスを変更しました"
+		  	flash[:notice] = "制作ステータスを変更しました"
 		    redirect_to admin_order_path(@order_detail.order)
 		  else
 		     render "show"

9e4a9f2006f83318dd0a8e9544496a2bcff11585
zakinao <zakinaororo@gmail.com>
2021-03-18 14:33:34 +0000

    change rspec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    customer = FactoryBot.build(:customer)
+  end
+  describe 'バリデーションのテスト' do
+    it 'ログインができる' do
+      sign_in customer
+      get customer_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end

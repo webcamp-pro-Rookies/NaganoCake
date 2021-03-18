4c45bb58777924e6f77cbee7c774725d8cbb5a94
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-18 12:16:00 +0000

    新規アイテム作成のセレクトフィールドの初期値を変更

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -20,7 +20,7 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_item_path(@item.id)
+      redirect_to item_path(@item.id)
     else
       render "new"
     end

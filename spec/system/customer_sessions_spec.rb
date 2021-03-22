0777c90dc1da6909527ea569ada5c67e0d7bbc3a
zakinao <zakinaororo@gmail.com>
2021-03-22 05:12:24 +0000

    管理者テスト完了

--- spec/system/customer_sessions_spec.rb
+++ spec/system/customer_sessions_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe "CustomerSessions", type: :system do
         telephone_number: '12345678912',
         address: '渋谷',
       )
+      @address = @customer.addresses.create(
+        name: '山本昌',
+        postal_code: '1234567',
+        address: '神奈川県'
+      )
       @genre = Genre.create(
         id: 1,
         name: "ケーキ"
@@ -126,5 +131,97 @@ RSpec.describe "CustomerSessions", type: :system do
       visit cart_items_path
       click_link '情報入力に進む'
       expect(current_path).to eq new_order_path
+    end
+    
+    context '注文情報入力画面' do
+      it '登録した住所が選択できること' do
+        visit item_path(@item)
+        select '1', from: 'amount'
+        find('.btn.btn-success.ml-3').click
+        click_link '情報入力に進む'
+        expect(page).to have_content('山本昌')
+      end
+      
+      it '注文確認画面へ遷移すること' do
+        visit item_path(@item)
+        select '1', from: 'amount'
+        find('.btn.btn-success.ml-3').click
+        click_link '情報入力に進む'
+        find('.btn.btn-primary.text-center').click
+        expect(current_path).to eq log_orders_path
+      end
+      
+      it '購入後サンクスページに遷移' do
+        visit item_path(@item)
+        select '1', from: 'amount'
+        find('.btn.btn-success.ml-3').click
+        click_link '情報入力に進む'
+        choose 'order_address_selection_addresses'
+        find('.btn.btn-primary.text-center').click
+        click_link '注文を確定する'
+        expect(current_path).to eq thanks_orders_path
+      end
+      
+      it 'サンクスページからトップページに遷移すること' do
+        visit item_path(@item)
+        select '1', from: 'amount'
+        find('.btn.btn-success.ml-3').click
+        click_link '情報入力に進む'
+        choose 'order_address_selection_addresses'
+        find('.btn.btn-primary.text-center').click
+        click_link '注文を確定する'
+        click_link 'NaganoCAKE'
+        expect(current_path).to eq root_path
+      end
+      
+      # it '購入時に登録した住所が表示される' do
+      #   visit item_path(@item)
+      #   select '1', from: 'amount'
+      #   find('.btn.btn-success.ml-3').click
+      #   click_link '情報入力に進む'
+      #   choose 'order_address_selection_new_address'
+      #   fill_in 'order_postal_code', with: '1237890'
+      #   fill_in 'order_address', with: '東京都渋谷区'
+      #   fill_in 'order_name', with: '松井秀喜'
+      #   find('.btn.btn-primary.text-center').click
+      #   click_link '注文を確定する'
+      #   click_link 'マイページ'
+      #   find('.shipping-btn.mr-2.btn.btn-primary.form-control').click
+      #   expect(page).to have_content('松井秀樹')
+      # end
+    end
+    
+    context '退会処理のテスト' do
+      it '退会画面に遷移する' do
+        click_link '編集する'
+        click_link '退会する'
+        expect(current_path).to eq quit_customers_path
+      end
+      
+      it '退会後トップ画面に遷移' do
+        click_link '編集する'
+        click_link '退会する'
+        click_link '退会する'
+        expect(current_path).to eq root_path
+      end
+      
+      it 'ヘッダが未ログイン状態' do
+        click_link '編集する'
+        click_link '退会する'
+        click_link '退会する'
+        expect(current_path).to eq root_path
+        expect(page).to have_content('ログイン')
+      end
+      
+      it '退会したアカウントのログイン不可' do
+        click_link '編集する'
+        click_link '退会する'
+        click_link '退会する'
+        click_link 'ログイン'
+        fill_in 'customer_email', with: @customer.email
+        fill_in 'customer_password', with: @customer.password
+        find('.btn.btn-block.btn-success').click
+        expect(page).to have_content('退会済のユーザーです')
+      end
     end
 end

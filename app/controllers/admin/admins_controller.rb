694280c0137e3d8ba48f943bd71afe8c2b55988c
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-23 05:07:30 +0000

    フォルダ構成を元に戻した

--- app/controllers/admin/admins_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::AdminsController < ApplicationController
-  # before_action :authenticate_customer!
-  # before_action redirect_to root unless user_signed_in?
-
-  # binding.pry
-  # test = "test"
-end
--- app/controllers/customer_controller.rb
+++
@@ -1,5 +0,0 @@
-class CustomerController < ApplicationController
-  # before_action :authenticate_customer!, unless: :admin_signed_in?
-  # before_action :authenticate_customer!, except: [:top, :about]
-
-end

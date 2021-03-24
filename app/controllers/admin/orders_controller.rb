3a112c7d9e429a9fe00d9de5803042b8df3b8461
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-24 04:46:09 +0000

    注文履歴詳細の各種ステータスの同期を強化

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -22,12 +22,19 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+
     @order = Order.find(params[:id])
+    status = Order.statuses[order_params[:status]]
+
     if @order.update(order_params)
 
-      if order_params[:status] == "入金確認"
+      if status >= Order.statuses["入金確認"]
         @order.order_details.each do |order_detail|
-          order_detail.update(making_status: 1) if order_detail.making_status == "着手不可"
+          order_detail.update(making_status: 1)
+        end
+      else
+        @order.order_details.each do |order_detail|
+          order_detail.update(making_status: 0)
         end
       end
 
